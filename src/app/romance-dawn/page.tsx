d9489097f8d5beb1e4c7533e744705d6964bad03
StiX1919 <devspencersmith@gmail.com>
2025-06-01 03:30:44 -0700

    hover card functionality v1

--- src/app/romance-dawn/page.tsx
+++ src/app/romance-dawn/page.tsx
@@ -6,12 +6,11 @@ import HoverCard from "./components/HoverCard";
 export default function Home() {
   const [factions, setFactions] = useState(['Pirates', 'Marines', 'Bounty Hunters']) 
 
-  const factionCards = factions.map((e, i) => (<HoverCard key={`${i}-${e}`} text={e} />))
+  const factionCards = factions.map((e, i) => (<HoverCard key={`${i}-${e}`} text={e} index={i} />))
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <div>Romance Dawn</div>
-        <HoverCard />
         {factionCards}
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
